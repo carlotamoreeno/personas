219ffb17151502be14c84f579c5d316f4f94e443
22061988 <22061988@C306-02.alumnos.uem.es>
2020-11-11 17:47:57 +0100

    ejercicio de personas

---
+++ src/personas/Dam.java
@@ -0,0 +1,24 @@
+package personas;
+
+public class Dam {
+
+	public static void main(String[] args) {
+
+		Persona miCarlota= new Persona();
+		Persona miSara= new Persona();
+		Persona miDiego= new Persona();
+		
+		miCarlota.setColorojos("marron");
+		miCarlota.setEstatura(170);
+		miCarlota.setPeso(55);
+		
+		miSara.setColorojos("RUBIO");
+		miSara.setEstatura(165);
+        miSara.setPeso(52);
+        
+        miDiego.setColorojos("azul");
+        miDiego.setEstatura(175);
+        miDiego.setPeso(65);
+	}
+
+}
---
+++ src/personas/Persona.java
@@ -0,0 +1,20 @@
+package personas;
+
+public class Persona {
+	
+	private String colorojos; // atributos private y no poner valor si no todas las personas igual.
+	private double estatura;
+	private double peso;
+	
+	public void setColorojos(String colorojos) {
+		this.colorojos = colorojos;
+	}
+	public void setEstatura(double estatura) {
+		this.estatura = estatura;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+	
+	}
